442e7588ded96a08ccdfac90a0dd17c0b26663c3
Michael Cazares <michaelcazares@gmail.com>
2024-07-23 10:46:50 -0700

    completed Exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a new instance of the Car class
+            Car myCar = new Car();
+
+            // Set values in the properties for the object
+            myCar.Make = "Toyota";
+            myCar.Model = "Camry";
+            myCar.Year = 2021;
+
+            // Print the values of each property to the Console
+            Console.WriteLine($"Make: {myCar.Make}");
+            Console.WriteLine($"Model: {myCar.Model}");
+            Console.WriteLine($"Year: {myCar.Year}");
         }
     }
+
+    // Create a class named Car - make sure it is public
+    public class Car
+    {
+        // Create a Make property of type string that is public
+        public string Make { get; set; } = default!;
+
+        // Create a Model property of type string that is public and required
+        public string Model { get; set; } = default!;
+
+        // Create a Year property of type int that is public
+        public int Year { get; set; }
+    }
 }
+
+
+/* Output
+Make: Toyota
+Model: Camry
+Year: 2021
+*/
